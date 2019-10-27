d9028bc271d24c34d10147f509ea2b1569ad7d72
michalkrzem <michal_krzem@wp.pl>
2019-10-27 11:01:28 +0100

    Problem z pobieraniem danych

--- analyse.py
+++ analyse.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
 
-df = pd.read_csv('2019-10-24.csv', sep=' ')
+df = pd.read_csv('2019-10-261.csv', sep=' ')
+df['Rok'] = df['Rok'].astype(str)
+df['Miesiac'] = df['Miesiac'].astype(str)
+df['Dzien'] = df['Dzien'].astype(str)
+
+
+df['Data_pomiaru'] = df['Rok'] + '-' + df['Miesiac'] + '-' + df['Dzien']
+
 print(df)
